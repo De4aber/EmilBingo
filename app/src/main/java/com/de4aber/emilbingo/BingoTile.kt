a5164a23bfc39e26495cf38b38e2475a92878849
Frann0 <deengo113@gmail.com>
2022-05-05 23:31:57 +0200

    Initial commit

---
+++ app/src/main/java/com/de4aber/emilbingo/BingoTile.kt
@@ -0,0 +1,6 @@
+package com.de4aber.emilbingo
+
+data class BingoTile (
+    val tekst: String,
+    var isClicked: Boolean
+)
---
+++ app/src/main/java/com/de4aber/emilbingo/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.de4aber.emilbingo
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        window.decorView.apply {systemUiVisibility = (View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_FULLSCREEN
+                or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY) }
+
+        var start = findViewById<Button>(R.id.Start);
+        var luk = findViewById<Button>(R.id.Luk);
+        start.setOnClickListener { onClickStart() }
+        luk.setOnClickListener { onClickLuk() }
+
+    }
+
+    fun onClickStart(){
+        val intent = Intent(this, GameActivity::class.java)
+        startActivity(intent);
+    }
+
+    fun onClickLuk(){
+        finish();
+    }
+}
